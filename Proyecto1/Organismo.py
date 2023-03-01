54d0b0a0d6627d3e88eb58e624c63a11f2355651
AnnFlets <analuciaflets@gmail.com>
2023-03-01 13:16:41 -0600

    Creacion clase Organismo - 1/3/2023

---
+++ Proyecto1/Organismo.py
@@ -0,0 +1,30 @@
+class Organismo:
+    def __init__(self, fila, columna, color, borde):
+        self.fila = fila
+        self.columna = columna
+        self.color = color
+        self.borde = borde
+
+    def getFila(self):
+        return self.fila
+
+    def setFila(self, fila):
+        self.fila = fila
+
+    def getColumna(self):
+        return self.columna
+
+    def setColumna(self, columna):
+        self.columna = columna
+
+    def getColor(self):
+        return self.Color
+
+    def setColor(self, color):
+        self.color = color
+
+    def getBorde(self):
+        return self.borde
+
+    def setBorde(self, borde):
+        self.borde = borde
